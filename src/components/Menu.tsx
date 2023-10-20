8db90df1e530bf66f02e513d1e1d5befcecec6c1
Elix Neto <neto.elix@outlook.com>
2023-10-20 17:14:11 -0300

    Criando componente de menu de navegação da pagina

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,6 +5,20 @@ function Menu() {
     <nav>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/projects">Projetos</NavLink>
+      <table>
+        <th>
+          <a href="https://www.linkedin.com/in/elixneto/" target="_blank" rel="noreferrer">Linkedin</a>
+        </th>
+        <th>
+          <a href="https://github.com/netoelix/" target="_blank" rel="noreferrer">GitHub</a>
+        </th>
+        <th>
+          <a href="https://www.instagram.com/elixneto/" target="_blank" rel="noreferrer">Instagram</a>
+        </th>
+        <th>
+          <a href="https://www.betrybe.com/" target="_blank" rel="noreferrer">Trybe</a>
+        </th>
+      </table>
     </nav>
   );
 }
