13f399541ebf26ecb01b738382a42a162e24bddd
Elix Neto <neto.elix@outlook.com>
2023-10-20 18:55:15 -0300

    Ornagizando os arquivos do projeto

--- src/components/Main/Home.tsx
+++ src/components/Main/Home.tsx
@@ -1,4 +1,5 @@
-import { BsInstagram, BsGithub, BsLinkedin, BsFillRocketTakeoffFill, BsRocket } from 'react-icons/bs';
+import { BsInstagram, BsGithub, BsLinkedin, BsFillRocketTakeoffFill, BsRocket }
+  from 'react-icons/bs';
 import Skills from './Skills';
 import About from './About';
 
@@ -7,23 +8,15 @@ function Home() {
     <main>
       <div>
         <h2>Elix Merlino Neto</h2>
-        <table>
-          <th>
-            <a href="https://www.linkedin.com/in/elixneto/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
-          </th>
-          <th>
-            <a href="https://github.com/netoelix/" target="_blank" rel="noreferrer"><BsGithub /></a>
-          </th>
-          <th>
-            <a href="https://www.instagram.com/elixneto/" target="_blank" rel="noreferrer"><BsInstagram /></a>
-          </th>
-          <th>
-            <a href="https://www.betrybe.com/" target="_blank" rel="noreferrer"><BsRocket /></a>
-          </th>
-        </table>
-        <About />
-        <Skills />
+        <nav>
+          <a href="https://www.linkedin.com/in/elixneto/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+          <a href="https://github.com/netoelix/" target="_blank" rel="noreferrer"><BsGithub /></a>
+          <a href="https://www.instagram.com/elixneto/" target="_blank" rel="noreferrer"><BsInstagram /></a>
+          <a href="https://www.betrybe.com/" target="_blank" rel="noreferrer"><BsRocket /></a>
+        </nav>
       </div>
+      <About />
+      <Skills />
     </main>
   );
 }
