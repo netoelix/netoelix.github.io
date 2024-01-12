a9b333b009a9067e15c4ff1f6235c964cfeb7990
Elix Neto <neto.elix@outlook.com>
2024-01-11 21:26:55 -0300

    Feat: arquivo data com as informações dos principais projetos

--- src/components/Main/Projects.tsx
+++ src/components/Main/Projects.tsx
@@ -1,19 +1,16 @@
-// import { NavLink } from 'react-router-dom';
-import data from '../../assets/data/data.json';
+import { data } from '../../assets/data/data';
 import { ProjectsContainer } from '../../styles/StiledProject';
 
 function Projetos() {
   return (
     <ProjectsContainer>
       <h2>Meus projetos</h2>
-      {data.map(({ id, about, technology, howToUse, resources }) => {
+      {data.map(({ id, location }) => {
         return (
           <article key={ id }>
             <h3>{id}</h3>
-            <p>{about}</p>
-            <p>{technology}</p>
-            <p>{howToUse}</p>
-            <p>{resources}</p>
+            <a href={ location }>Repositório</a>
+            <p>Em construção</p>
           </article>
         );
       })}
