9dbcce5abc4ed8bbd9f41535369b0c4ab93d9ab2
Elix Neto <neto.elix@outlook.com>
2024-01-13 02:30:14 -0300

    Fix: imagens da pagina

--- src/components/Main/Home.tsx
+++ src/components/Main/Home.tsx
@@ -1,13 +1,14 @@
 import About from './About';
 import { HomeContainer, SectionContainer, ImgContainer } from '../../styles/StiledHome';
 import SocialMedia from './SocialMedia';
+import profilePicture from '../../assets/imagens/profile.jpg';
 
 function Home() {
   return (
     <HomeContainer>
       <SectionContainer>
         <ImgContainer>
-          <img src="src/assets/imagens/profile.jpg" alt="profile" />
+          <img src={ profilePicture } alt="profile" />
           <section>
             <h2>Elix Merlino Neto</h2>
             <h3>Desenvolvedor Web</h3>
